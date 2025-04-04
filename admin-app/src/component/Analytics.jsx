4bde4158b6ca43df3862a110ba8991ac01b3792a
Lê Tấn Phong <141659740+LeTanPhong3003@users.noreply.github.com>
2025-04-04 18:29:13 +0700

    Bai 11: Css app

--- admin-app/src/component/Analytics.jsx
+++ admin-app/src/component/Analytics.jsx
@@ -6,13 +6,20 @@ const Analytics = () => {
   return (
     <div className="grid-container">
       <div className="grid-item item1">
-        LOGO
+        <div className="logo-container">
+          <img src="/img/logo.jpg" alt="Logo" className="logo" />
+        </div>
         <NavLink
           to="/dashboard"
           className={({ isActive }) =>
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/dashboard.png"
+            alt="Dashboard Icon"
+            className="nav-icon"
+          />
           Dashboard
         </NavLink>
         <NavLink
@@ -21,6 +28,11 @@ const Analytics = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/project.png"
+            alt="Projects Icon"
+            className="nav-icon"
+          />
           Projects
         </NavLink>
         <NavLink
@@ -29,6 +41,7 @@ const Analytics = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img src="/img/team.png" alt="Teams Icon" className="nav-icon" />
           Teams
         </NavLink>
         <NavLink
@@ -37,6 +50,11 @@ const Analytics = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/analytic.png"
+            alt="Analytics Icon"
+            className="nav-icon"
+          />
           Analytics
         </NavLink>
         <NavLink
@@ -45,7 +63,12 @@ const Analytics = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
-          Message
+          <img
+            src="/img/message.png"
+            alt="Messages Icon"
+            className="nav-icon"
+          />
+          Messages
         </NavLink>
         <NavLink
           to="/integrations"
@@ -53,19 +76,35 @@ const Analytics = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/integration.png"
+            alt="Integrations Icon"
+            className="nav-icon"
+          />
           Integrations
         </NavLink>
       </div>
       <div className="grid-item item2 header">
         <h1 className="header-title">Analytics</h1>
         <div className="header-actions">
-          <input type="text" placeholder="Search..." className="search-input" />
+          <div className="search-input-container">
+            <img
+              src="./img/search.png"
+              alt="Search Icon"
+              className="search-icon"
+            />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-input"
+            />
+          </div>
           <img
-            src="/img/notification.jpg"
+            src="/img/notification.png"
             alt="Notification"
             className="notification-icon"
           />
-          <img src="/img/help.jpg" alt="Help" className="help-icon" />
+          <img src="/img/help.png" alt="Help" className="help-icon" />
           <img
             src="https://loremflickr.com/40/40?lock=1"
             alt="User Avatar"
