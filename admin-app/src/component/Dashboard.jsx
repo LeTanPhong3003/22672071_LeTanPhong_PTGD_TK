f96164aabaf38027122ebf6fea657f0ba0e36356
Lê Tấn Phong <141659740+LeTanPhong3003@users.noreply.github.com>
2025-04-04 18:29:13 +0700

    Bai 11: Css app

--- admin-app/src/component/Dashboard.jsx
+++ admin-app/src/component/Dashboard.jsx
@@ -239,13 +239,20 @@ const GridLayout = () => {
   return (
     <div className="grid-container">
       <div className="grid-item item1">
-        LOGO
+        <div className="logo-container">
+          <img src="/img/logo.jpg" alt="Logo" className="logo" />
+        </div>
         <NavLink
           to="/dashboard"
           className={({ isActive }) =>
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/dashboard.png"
+            alt="Dashboard Icon"
+            className="nav-icon"
+          />
           Dashboard
         </NavLink>
         <NavLink
@@ -254,6 +261,11 @@ const GridLayout = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/project.png"
+            alt="Projects Icon"
+            className="nav-icon"
+          />
           Projects
         </NavLink>
         <NavLink
@@ -262,6 +274,7 @@ const GridLayout = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img src="/img/team.png" alt="Teams Icon" className="nav-icon" />
           Teams
         </NavLink>
         <NavLink
@@ -270,6 +283,11 @@ const GridLayout = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/analytic.png"
+            alt="Analytics Icon"
+            className="nav-icon"
+          />
           Analytics
         </NavLink>
         <NavLink
@@ -278,7 +296,12 @@ const GridLayout = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
-          Message
+          <img
+            src="/img/message.png"
+            alt="Messages Icon"
+            className="nav-icon"
+          />
+          Messages
         </NavLink>
         <NavLink
           to="/integrations"
@@ -286,19 +309,35 @@ const GridLayout = () => {
             isActive ? "active-link" : "inactive-link"
           }
         >
+          <img
+            src="/img/integration.png"
+            alt="Integrations Icon"
+            className="nav-icon"
+          />
           Integrations
         </NavLink>
       </div>
       <div className="grid-item item2 header">
         <h1 className="header-title">Dashboard</h1>
         <div className="header-actions">
-          <input type="text" placeholder="Search..." className="search-input" />
+          <div className="search-input-container">
+            <img
+              src="./img/search.png"
+              alt="Search Icon"
+              className="search-icon"
+            />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-input"
+            />
+          </div>
           <img
-            src="/img/notification.jpg"
+            src="/img/notification.png"
             alt="Notification"
             className="notification-icon"
           />
-          <img src="/img/help.jpg" alt="Help" className="help-icon" />
+          <img src="/img/help.png" alt="Help" className="help-icon" />
           <img
             src="https://loremflickr.com/40/40?lock=1"
             alt="User Avatar"
@@ -318,35 +357,68 @@ const GridLayout = () => {
           </h2>
         </div>
         <div className="nested-grid-container">
-          <div className="nested-grid-item">
+          <div className="nested-grid-item turnover">
             <div className="item-header">
               <p className="title">Turnover</p>
-              <button className="icon-button">
+              <div className="icon-button">
                 <img src="./img/turnover.jpg" alt="Turnover Icon" />
-              </button>
+              </div>
             </div>
             <p className="data">${data.turnover}</p>
-            <p className="change">{data.turnoverChange}% period of change</p>
+            <p
+              className={`change ${
+                data.turnoverChange >= 0 ? "positive" : "negative"
+              }`}
+            >
+              <span
+                className={`change-icon ${
+                  data.turnoverChange >= 0 ? "up" : "down"
+                }`}
+              ></span>
+              {data.turnoverChange}% period of change
+            </p>
           </div>
-          <div className="nested-grid-item">
+          <div className="nested-grid-item profit">
             <div className="item-header">
               <p className="title">Profit</p>
-              <button className="icon-button">
+              <div className="icon-button">
                 <img src="./img/profit.jpg" alt="Profit Icon" />
-              </button>
+              </div>
             </div>
             <p className="data">${data.profit}</p>
-            <p className="change">{data.profitChange}% period of change</p>
+            <p
+              className={`change ${
+                data.profitChange >= 0 ? "positive" : "negative"
+              }`}
+            >
+              <span
+                className={`change-icon ${
+                  data.profitChange >= 0 ? "up" : "down"
+                }`}
+              ></span>
+              {data.profitChange}% period of change
+            </p>
           </div>
-          <div className="nested-grid-item">
+          <div className="nested-grid-item new-customer">
             <div className="item-header">
               <p className="title">New Customer</p>
-              <button className="icon-button">
+              <div className="icon-button">
                 <img src="./img/customer.jpg" alt="Customer Icon" />
-              </button>
+              </div>
             </div>
             <p className="data">{data.newCustomer}</p>
-            <p className="change">{data.newCustomerChange}% period of change</p>
+            <p
+              className={`change ${
+                data.newCustomerChange >= 0 ? "positive" : "negative"
+              }`}
+            >
+              <span
+                className={`change-icon ${
+                  data.newCustomerChange >= 0 ? "up" : "down"
+                }`}
+              ></span>
+              {data.newCustomerChange}% period of change
+            </p>
           </div>
         </div>
       </div>
